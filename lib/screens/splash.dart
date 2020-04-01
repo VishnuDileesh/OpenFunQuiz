ebbacad95d94d12d91a6a9c48c931e3ccf0f91f7
Pheyonix <dev@vishnudileesh.com>
2020-04-01 16:44:35 +0530

    Set timer for Splash Screen

--- lib/screens/splash.dart
+++ lib/screens/splash.dart
@@ -1,6 +1,30 @@
 import 'package:flutter/material.dart';
+import 'dart:async';
+import 'home.dart';
 
-class SplashScreen extends StatelessWidget{
+class SplashScreen extends StatefulWidget{
+	@override
+	_SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen>{
+
+	@override
+	void initState(){
+		super.initState();
+
+
+		Timer(Duration(seconds: 3), (){
+			Navigator.push(
+				context,
+				MaterialPageRoute(
+					builder: (context) => HomeScreen()
+				),
+			);
+		});
+
+	}
+
 	@override
 	Widget build(BuildContext context){
 		return Material(
